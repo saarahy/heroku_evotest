393f05b5393fc8ea26911ba205640abb26c49031
saarahy <juarez.s.perla@gmail.com>
2017-05-15 10:37:57 -0700

    editing files

--- evospace.py
+++ evospace.py
@@ -11,8 +11,8 @@ RESPAWN='REINSERT'
 
 HOST="redis-10326.c8.us-east-1-3.ec2.cloud.redislabs.com"
 #"pub-redis-13994.us-east-1-3.3.ec2.garantiadata.com"#"pub-redis-17694.us-east-1-3.4.ec2.garantiadata.com"
-PORT=10326#13994#17694
-PASS="evo6"#"evopool6"
+PORT = 10326#13994#17694
+PASS = "evo6"#"evopool6"
 import os, redis, random
 
 ##REDISCLOUD
@@ -23,9 +23,7 @@ if os.environ.get('REDISTOGO_URL'):
     r = redis.Redis(host=url.hostname, port=url.port, password=url.password)
 #LOCAL
 else:
-    r = redis.Redis(host=HOST, port=PORT,password=PASS)
-
-
+    r = redis.Redis(host=HOST, port=PORT, password=PASS)
 
 class Individual:
     def __init__(self, **kwargs):
@@ -65,13 +63,12 @@ class Individual:
         else:
             return self
 
-
     def __repr__(self):
-        return self.id +":"+ str(self.fitness) +":" + str( self.chromosome)
-
+        return self.id + ":" + str(self.fitness) + ":" + str(self.chromosome)
 
     def as_dict(self):
         return self.__dict__
+
 
 class Specie:
     def __init__(self, **kwargs):
@@ -89,8 +86,9 @@ class Specie:
     def as_dict(self):
         return self.__dict__
 
+
 class Population:
-    def __init__(self, name = "pop" ):
+    def __init__(self, name="pop"):
         self.name = name
         self.sample_counter = self.name+':sample_count'
         self.individual_counter = self.name+':individual_count'
@@ -99,26 +97,21 @@ class Population:
         self.returned_counter = self.name+":returned_count"
         self.log_queue = self.name+":log_queue"
 
-
     def get_returned_counter(self):
-        return int( r.hget('at', self.returned_counter))
-
+        return int(r.hget('at', self.returned_counter))
 
     def individual_next_key(self):
         key = r.hincrby('at', self.individual_counter)
         return self.name+":individual:%s" % key
 
-
     def size(self):
         return r.hkeys(self.name)
-
 
     def initialize(self):
         pattern = "%s*" % self.name
         keys = r.keys(pattern)
         if keys:
             r.delete(*keys)
-
         #r.flushall()
         r.hsetnx('at', self.sample_counter, 0)
         r.hsetnx('at', self.individual_counter, 0)
@@ -156,7 +149,7 @@ class Population:
         return result
 
     def get_species(self):
-        l_k=[]
+        l_k = []
         keys = sorted(r.keys())
         #keys.pop()
         for e in keys:
@@ -172,11 +165,9 @@ class Population:
             self.respawn(RE_INSERT_SAMPLES)
 
         sample_id = r.incr(self.sample_counter)
-
-        #Get keys
+        # Get keys
         sample = [r.spop(self.name) for i in range(size)]
-
-        #If there is a None
+        # If there is a None
         if None in sample:
             sample = [s for s in sample if s]
             if not sample:
@@ -184,21 +175,17 @@ class Population:
         r.sadd(self.name+":sample:%s" % sample_id, *sample)
         r.rpush(self.sample_queue, self.name+":sample:%s" % sample_id)
         try:
-            result =  {'sample_id': self.name+":sample:%s" % sample_id ,
+            result ={'sample_id': self.name+":sample:%s" % sample_id,
                        'sample':   [Individual(id=key).get(as_dict=True) for key in sample ]}
         except:
             return None
         return result
 
     def get_sample_specie(self, specie):
-
         if AUTO_RESPAWN and r.scard(self.name) <= MIN_SIZE:
             self.respawn(RE_INSERT_SAMPLES)
 
         sample_id = r.hincrby('at', self.sample_counter)
-
-            # Get keys
-
         sample2 = []
         sample = r.hgetall(specie)
         for key in sample:
@@ -208,7 +195,7 @@ class Population:
             sample = [s for s in sample if s]
             if not sample:
                 return None
-        #r.sadd(self.name + ":sample:%s" % sample_id, *sample)
+        # r.sadd(self.name + ":sample:%s" % sample_id, *sample)
         r.rpush(self.sample_queue, self.name + ":sample:%s" % sample_id)
         try:
             result = {'sample_id': self.name + ":sample:%s" % sample_id,
@@ -217,26 +204,22 @@ class Population:
             return None
         return result
 
-
     def read_sample_queue(self):
         result = r.lrange(self.sample_queue,0,-1)
         return result
 
-
     def read_sample_queue_len(self):
         return r.llen(self.sample_queue)
-
 
     def read_pop_keys(self):
         sample = r.smembers(self.name)
         sample = list(sample)
-        result =  { 'sample': sample }
+        result = {'sample': sample}
         return result
-
 
     def read_all(self):
         sample = r.smembers(self.name)
-        result =  { 'sample':   [Individual(id=key).get(as_dict=True) for key in sample]}
+        result = {'sample':   [Individual(id=key).get(as_dict=True) for key in sample]}
         return result
 
     def put_specieinfo(self, **kwargs):
@@ -245,18 +228,17 @@ class Population:
         specie = Specie(**kwargs)
         specie.put(self.id)
 
-
-    def put_individual(self, **kwargs ):
+    def put_individual(self, **kwargs):
         if kwargs['id'] is None:
             kwargs['id'] = self.name+":individual:%s" % r.hincrby('at',self.individual_counter)
         ind = Individual(**kwargs)
         ind.put(self.name)
 
     def put_sample(self, sample):
-        if not isinstance(sample,dict):
+        if not isinstance(sample, dict):
             raise TypeError("Samples must be dictionaries")
 
-        r.hincrby('at',self.returned_counter)
+        r.hincrby('at', self.returned_counter)
 
         if LOGGING:
             count = r.hincrby('at',self.returned_counter)
@@ -293,21 +275,19 @@ class Population:
         r.delete(sample['sample_id'])
         r.lrem(self.sample_queue, sample['sample_id'])
 
-
     def respawn_sample(self, sample_id):
         if r.exists(sample_id):
             members = r.smembers(sample_id)
             r.sadd(self.name, *members)
             r.delete(sample_id)
-            r.lrem(self.sample_queue,sample_id,1)
+            r.lrem(self.sample_queue, sample_id, 1)
 
-    def respawn_ratio(self, ratio = .2):
-        until_sample  = int(r.llen(self.sample_queue)*ratio)
+    def respawn_ratio(self, ratio=.2):
+        until_sample = int(r.llen(self.sample_queue)*ratio)
         for i in range(until_sample):
             self.respawn_sample(r.lpop(self.sample_queue))
 
-
-    def respawn(self, n = 1):
+    def respawn(self, n=1):
         if RESPAWN == 'REINSERT':
             current_size = r.llen(self.sample_queue)
             if n > current_size:
@@ -321,6 +301,4 @@ class Population:
         return r.get(self.name+":found")
 
     def found_it(self):
-        r.set(self.name+":found",1)
-
-
+        r.set(self.name+":found", 1)
