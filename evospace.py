0d1e437f65dc628ab392b06a7fa1f270794c886e
saarahy <juarez.s.perla@gmail.com>
2017-10-06 01:57:21 -0700

    modifying - inserting specie atts

--- evospace.py
+++ evospace.py
@@ -10,9 +10,9 @@ RESPAWN='REINSERT'
 #RESPAWN='RANDOM'
 
 
-HOST="redis-10326.c8.us-east-1-3.ec2.cloud.redislabs.com"
+HOST="redis-17601.c10.us-east-1-3.ec2.cloud.redislabs.com"
 # "pub-redis-13994.us-east-1-3.3.ec2.garantiadata.com"#"pub-redis-17694.us-east-1-3.4.ec2.garantiadata.com"
-PORT = 10326#13994#17694
+PORT = 17601#13994#17694
 PASS = "evo6"#"evopool6"
 
 # HOST= "localhost"
